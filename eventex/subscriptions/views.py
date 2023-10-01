58f64ff19844015365ad40d8cd5fcfe8ea2a8536
Cadú <139708523+mrcadu7@users.noreply.github.com>
2023-10-01 16:23:20 -0300

    persiste a inscrição

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -5,6 +5,7 @@ from django.core import mail
 from django.template.loader import render_to_string
 from django.contrib import messages
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -27,6 +28,7 @@ def create(request):
                'subscriptions/subscription_email.txt',
                form.cleaned_data)
 
+    Subscription.objects.create(**form.cleaned_data)
     # Success feedback
     messages.success(request, 'Inscrição realizada com sucesso!')
 
